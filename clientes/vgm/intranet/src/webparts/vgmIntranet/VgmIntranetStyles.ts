dc07a3817e4cee784a042a5dc740393aa4953302
Wladimick <61322487+wladimick@users.noreply.github.com>
2026-09-02 17:51:04 -0400

    feat(vgm): include product administration styles

--- clientes/vgm/intranet/src/webparts/vgmIntranet/VgmIntranetStyles.ts
+++ clientes/vgm/intranet/src/webparts/vgmIntranet/VgmIntranetStyles.ts
@@ -1,4 +1,5 @@
 import { VGM_BASE_STYLES } from './VgmBaseStyles';
 import { VGM_ENHANCEMENT_STYLES } from './VgmEnhancementStyles';
+import { VGM_PRODUCT_STYLES } from './VgmProductStyles';
 
-export const VGM_STYLES: string = `${VGM_BASE_STYLES}\n${VGM_ENHANCEMENT_STYLES}`;
+export const VGM_STYLES: string = `${VGM_BASE_STYLES}\n${VGM_ENHANCEMENT_STYLES}\n${VGM_PRODUCT_STYLES}`;
